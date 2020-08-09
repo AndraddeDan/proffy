b5346e6473da10e8fc869c564c8205e767fb02a7
Daniel Andrade <andraddedan@outlook.com>
2020-08-09 11:15:59 -0300

    adding api connections

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -6,8 +6,9 @@ const Select: React.FC<Props> = ({ label, name, options, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest} defaultValue={'default'}>
-        <option value="default" disabled hidden>Selecione uma opção</option>
+      <select value="" id={name} {...rest}>
+        <option value="" disabled hidden>Selecione uma opção</option>
+
         {options.map((option, index) =>
           (<option key={index} value={option.value}>{option.label}</option>))}
       </select>
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import backgroundLogo from '../../assets/img/logo.svg';
@@ -10,7 +10,19 @@ import purpleHeartIcon from '../../assets/icons/purple-heart.svg';
 
 import './style.scss';
 
+import api from '../../services/api';
+
 function Home() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -35,7 +47,7 @@ function Home() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração Roxo" />
         </span>
 
