35c2c43af1ef2af05248934ce08425f0a9206f4b
Daniel Andrade <andraddedan@outlook.com>
2020-08-09 12:51:49 -0300

    teacher list item component props

--- web/src/components/TeacherListItem/props.ts
+++ web/src/components/TeacherListItem/props.ts
@@ -1,2 +1,12 @@
 export interface Props {
+  teacher: {
+    id: number;
+    user_id: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+    subject: string;
+    cost: number;
+  };
 }
