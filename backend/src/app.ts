275f654ec104a0d71b70647f11ba461fc0e84080
Daniel Andrade <andraddedan@outlook.com>
2020-08-08 23:04:04 -0300

    cors and routes added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 import index from './routes';
-import users from './routes/users';
+import connections from './routes/connections';
+import classes from './routes/classes';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 
 app.use('/api/v1', index);
-app.use('/api/v1/users', users);
+app.use('/api/v1/connections', connections);
+app.use('/api/v1/classes', classes);
 
 export default app;
